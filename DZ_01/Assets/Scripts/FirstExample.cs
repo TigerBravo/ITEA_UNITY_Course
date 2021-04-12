714b13e36ffdd350ffa6e6385235beabcd29bdcb
Alex Molotov <petroffbio2@gmail.com>
2021-04-12 04:42:16 +0300

    add homework

---
+++ DZ_01/Assets/Scripts/FirstExample.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstExample : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f;
+    Vector2 vel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Controll();
+        Controll2();
+    }
+
+    private void Controll()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Movement(Vector3.forward);      
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Movement(Vector3.back);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Movement(Vector3.left);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Movement(Vector3.right);  
+        }
+    }
+
+    private void Controll2()
+    {
+        vel.y = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+        vel.x = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+        transform.Translate(vel.x, 0, vel.y);
+
+    }
+    private void Movement(Vector3 direction)
+    {
+
+        transform.position += direction * _speed * Time.deltaTime;
+        
+    }
+}
---
+++ DZ_01/Assets/Scripts/Third_Person_Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Third_Person_Movement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    [SerializeField] private float speed = 6f;
+    //плавность поворота
+    [SerializeField] private float turnSmoothTime = 0.1f;
+    [SerializeField] private float turnSmoothVelocity; 
+
+
+    void Start()
+    {
+    }
+ 
+    void FixedUpdate()
+    {
+        Controll();
+    }
+    private void Controll()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        if (direction.magnitude >= 0.1f)
+        {
+            //поворот
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            //плавность поворота
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+
+    }
+
+
+}
